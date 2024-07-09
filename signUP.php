bb915498beb91be2ba8b98cec7356830fee19bef
John Walshe <walshejohnnyw7@gmail.com>
2024-07-09 23:05:55 +0100

    :lipstick: Add styling to sign up form

--- signUP.php
+++ signUP.php
@@ -2,16 +2,21 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+<link rel="stylesheet" href="static/css/signUP.css">
 	<title>Sign Up</title>
 </head>
 <body>
 	<div class="form">
+        <div class="title">
+            <p>Sign UP form</p>
+        </div>
 	<form action="" method='post'>
             <input type='text' name="name" placeholder='Name'>
             <input type='text' name="lastName" placeholder='Last Name'>
             <input type='text' name="email" placeholder='Email'>
             <input type='text' name="password" placeholder='Password'>
             <input type='text' name="confiPassword" placeholder='Confirm Password'>
+            <input type='submit' value="Sign UP" name='signUP_button'>
         </form>
         </div>
 </body>
