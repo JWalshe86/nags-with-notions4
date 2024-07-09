f851e6038f074cce783c711100660fff1410ce47
John Walshe <walshejohnnyw7@gmail.com>
2024-07-09 22:23:17 +0100

    :construction: Add connection.php

---
+++ signUP.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sign Up</title>
+</head>
+<body>
+	<div class="form">
+	<form action="" method='post'>
+            <input type='text' name="name" placeholder='Name'>
+            <input type='text' name="lastName" placeholder='Last Name'>
+            <input type='text' name="email" placeholder='Email'>
+            <input type='text' name="password" placeholder='Password'>
+            <input type='text' name="confiPassword" placeholder='Confirm Password'>
+        </form>
+        </div>
+</body>
+</html>
