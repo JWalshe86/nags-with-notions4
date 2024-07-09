d760323ae7360bc3cc5091defb6e9b2e5f241b08
John Walshe <walshejohnnyw7@gmail.com>
2024-07-09 23:50:42 +0100

    :lipstick: Add style to login.php

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+<link rel="stylesheet" href="static/css/signUP.css">
+	<title>Login</title>
+<style>
+    .form{
+    width: 230px;
+    height: 280px;
+
+}
+</style>
+</head>
+<body>
+	<div class="form">
+        <div class="title">
+            <p>Login form</p>
+        </div>
+	<form action="" method='post'>
+            <input type='text' name="name" placeholder='Name'>
+            <input type='text' name="password" placeholder='Password'>
+            <input type='submit' value="login" name='login_button'>
+        </form>
+        </div>
+</body>
+</html>
